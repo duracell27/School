cc6c0afd6a5bf8087953dc404248bedb3464459b
Duracell27 <volodymyrshmidt@gmail.com>
2026-04-20 12:09:44 +0300

    feat: add children DTOs

---
+++ back/src/children/dto/create-child.dto.ts
@@ -0,0 +1,62 @@
+import {
+  IsString,
+  IsInt,
+  IsOptional,
+  IsArray,
+  IsNotEmpty,
+  IsDateString,
+  Min,
+  ValidateNested,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class ParentContactDto {
+  @IsString()
+  @IsNotEmpty()
+  label: string;
+
+  @IsString()
+  @IsNotEmpty()
+  phone: string;
+}
+
+export class CreateChildDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsInt()
+  @Min(1)
+  @Type(() => Number)
+  age: number;
+
+  @IsString()
+  @IsOptional()
+  country?: string;
+
+  @IsString()
+  @IsOptional()
+  avatar?: string;
+
+  @IsDateString()
+  @IsOptional()
+  hireDate?: string;
+
+  @IsDateString()
+  @IsOptional()
+  graduationDate?: string;
+
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => ParentContactDto)
+  @IsOptional()
+  parentContacts?: ParentContactDto[];
+
+  @IsString()
+  @IsNotEmpty()
+  timezone: string;
+
+  @IsString()
+  @IsOptional()
+  teacherId?: string;
+}
